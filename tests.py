40e5436be8bf2f4bf6770449fecd477c0f8487a6
vicky <victoria.awuor@student.moringaschool.com>
2022-02-19 16:08:05 +0300

    multiple test to test if we can save multiple credentials in the list

--- tests.py
+++ tests.py
@@ -41,12 +41,6 @@ class TestCredentials(unittest.TestCase):
         '''
         self.new_credentials = Credentials ("twitter","james", "eve") #created credentials object
 
-    def tearDown(self):
-            '''
-            tearDown method that does clean up after each test case has run.
-            '''
-            Credentials.credentials_list = []
-
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly.
@@ -54,6 +48,30 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.appname,"twitter")
         self.assertEqual(self.new_credentials.username,"james")
         self.assertEqual(self.new_credentials.password,"eve")
+
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Credentials.credentials_list = []
+
+    def test_save_credentials(self):
+        '''
+        test_save_credentials test case to test if the credentials object is saved into
+         the credentials list
+        '''
+        self.new_credentials.save_credentials() # saving the new credentials
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+    def test_save_multiple_credentials(self):
+            '''
+            test_save_multiple_credentials to check if we can save multiple credentials
+            objects to our credentials_list
+            '''
+            self.new_credentials.save_credentials()
+            test_credentials = Credentials("twitter","james","eve") # new credentials
+            test_credentials.save_credentials()
+            self.assertEqual(len(Credentials.credentials_list),2)
 
 
 if __name__ ==  '__main__':
