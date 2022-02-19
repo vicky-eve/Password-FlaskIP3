187d36384b18ffd232c46641a128ee91441b9d25
vicky <victoria.awuor@student.moringaschool.com>
2022-02-19 12:11:00 +0300

    passed new instances of the object class

--- user.py
+++ user.py
@@ -1,5 +1,14 @@
-class User
-"""
-class that generates new instances of the user
-"""
-def __init__(self,password):
+class User:
+    """
+    class that generates new instances of the user.
+    """
+
+def __init__(self,username,password):
+
+     '''
+     init method to define properties for our methods.
+
+     Args:
+     username:New user username.
+     password:New user password.
+     '''
