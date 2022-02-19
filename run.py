87c1d6dee1d5f74bd22a3a5d1a7ed3b8496633a2
vicky <victoria.awuor@student.moringaschool.com>
2022-02-19 18:43:12 +0300

    log in and sign in options

--- run.py
+++ run.py
@@ -41,3 +41,39 @@ def del_credentials(credentials):
     Function to delete a credential
     '''
     credentials.delete_credentials()
+
+def main():
+    print("Oh Hello There! Welcome to the Password app! Where you get to save your secrets")
+    username = input ("Enter your username")
+
+    while True:
+        print(f"Hi there {username}, use either of these codes to sign or log in")
+        print("sn - sign up to new users")
+        print("lg - log into your account")
+        short_code = input("Enter short code:").upper()
+
+        if short_code == 'SN':
+            print("Enter yout username: ")
+            username = input()
+
+            print("Enter your desired password")
+            fpin = input()
+            print('*' * 60)
+
+            print("Successful registration!")
+            print("Now log into your account",)
+            print('\n')
+
+        elif short_code == 'LG':
+            print("Enter your username")
+            username = input()
+
+            print("Enter your password")
+            password= input()
+            if f"{username } = {password}":
+                print('*' * 10)
+
+                print(f"{username}, you've succesfully logged into your account")
+                print('\n')
+
+                pass
