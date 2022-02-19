b7b5c41e7938daa90dee2dbdf9cdfd8850324d45
vicky <victoria.awuor@student.moringaschool.com>
2022-02-19 14:49:24 +0300

    testing if our objects are being instantiated correctly

--- tests.py
+++ tests.py
@@ -1,2 +1,21 @@
 import unittest #importing unittest module
 from user import User #importing the user class
+
+class TestUser(unittest.TestCase)
+    '''
+    Test class that defines test cases for the user class behaviors.
+    Args:
+        unittest.TestCase:testcase class that helps in creating test cases.
+    '''
+    def setUp (self):
+        '''
+        set up method before each test case
+        '''
+        self.new_user = User ("james","eve") #created user object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly.
+        '''
+        self.assertEqual(self.new_user.username,"james")
+        self.assertEqual(self.new_user.password,"eve")
